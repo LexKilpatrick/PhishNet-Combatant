5bedc561d07ae865ca60a50bcc018490bb46b5ef
Giovanill <giovani.giagnacovo@gmail.com>
2024-01-28 02:24:40 -0600

    My second git commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,13 @@ function App() {
         <div class="gradient-text"><h1 class="name">P H I S H N E T</h1></div>
       </div>
       <p className="mission">PhishNet is an online web application that was designed to spread awareness.</p>
-      <button onClick={showMessage}>Process</button>
+
       
+      {/* Large textarea for lengthy email input */}
+      <div className="input-container">
+        <textarea placeholder="Type your email here..."></textarea>
+      </div>
+      <button onClick={showMessage}>Process</button>
     </div>
   );
 }
